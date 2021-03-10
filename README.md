404f6a6331f15f851a0023047f346f1a641bc1b4
syd333 <sydmiranda@icloud.com>
2021-03-10 14:56:32 -0800

    fixed bugs, block created and persisting

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # simulationfrontend
 
 //TODO:::
-- make sure routes + controllers are right
-- set up HTML 
+- append post for block []
+- patch for block or delete for channel []
+- delete for block []
+- fix delete btn (deleting every block when 1st is  clicked) []
+- clicking simulation in nav takes u to home []
+
+STRETCH:::
+- CSS
 - turn fetch to axios
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const BASE_URL = "http://localhost:3000"
 const BLOCKS_URL = `${BASE_URL}/blocks`
 const CHANNELS_URL = `${BASE_URL}/channels`
+// const CHANNELSARR = []
+// everytime a channel it gets pushed into an array
+// function with array then iterates over array 
+// grabs the select/options to populate for block form
 
 document.addEventListener("DOMContentLoaded", function() {
     getChannels()
@@ -8,7 +12,10 @@ document.addEventListener("DOMContentLoaded", function() {
 function getChannels(){
     fetch(CHANNELS_URL)
     .then(res => res.json())
-    .then(channels => channels.forEach(channel => buildChannel(channel)))
+    .then(channels => {
+        channels.forEach(channel => buildChannel(channel))
+        buildBlockFormDropDown(channels)
+    })
     .then(getBlocks())
 }
 
@@ -30,15 +37,25 @@ function getBlocks(){
     .then(blocks => blocks.forEach(block => buildBlock(block)))
 }
 
+function postBlock(block){
+    fetch(BLOCKS_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/json',
+        },
+        body: JSON.stringify(block)
+    })
+    .then(res => res.json())
+    .then(block => buildBlock(block))
+
+}
+
 function buildChannel(channel){
         const container = document.querySelector('.channel-container') 
         const card = document.createElement('card')
         const p = document.createElement('p')
-        let blockDiv = document.createElement('div')
-        let blockContainer = document.querySelector('.block-container')
 
         p.dataset.id = channel.id
-        blockDiv.id = channel.id
         container.className = 'channel-container'
         card.className = 'card-body-channel'
         p.className = 'channel-title'
@@ -47,89 +64,144 @@ function buildChannel(channel){
         container.appendChild(card)
         card.appendChild(p)
 
-        card.addEventListener('click', (e) => showHideDiv(e, p.dataset.id))
-        blockContainer.appendChild(blockDiv)
-        }
+        card.addEventListener('click', (e) => {
+        //   return showHideDiv(e) 
+        // console.log(p.parentElement)
+        })
+    }
 
+let blockContainer = document.querySelector('.block-container')
 function buildBlock(block){
-    // console.dir(document.querySelector('.block-container'))
-    // let div = await function() {
-    //    return document.getElementById(26)
-    // }
+    if (document.getElementById(block.channel_id) === null ) {
+            let blockDiv = document.createElement('div')
+            const containerB = document.createElement('div')
+            const cardB = document.createElement('card')
+            const h5 = document.createElement('h5')
+            const p = document.createElement('p')
+            // const deleteBtn = document.createElement('button')
+        
+            
+            containerB.className = 'block-container'
+            containerB.id = block.channel_id
+            // containerB.dataset.id = block.id
+            cardB.className = 'card-body-block'
+            h5.className = 'card-title'
+            p.className = 'card-text'
+            h5.textContent = block.title
+            p.innerText = block.description
+            // deleteBtn.innerText = 'x'
+        
+            // blockDiv.hidden = false
+            cardB.append(h5, p)
+            containerB.appendChild(cardB)
+            blockDiv.append(containerB)
+            blockContainer.append(blockDiv)
 
-    console.log(div())
+            // deleteBtn.addEventListener('click', deleteBlock(block, containerB.dataset.id))
 
-    const containerB = document.createElement('div') 
-    const cardB = document.createElement('card')
-    const h5 = document.createElement('h5')
-    const p = document.createElement('p')
+    } else {
+        let blockDiv = document.getElementById(block.channel_id)
+        const containerB = document.createElement('div') 
+        const cardB = document.createElement('card')
+        const h5 = document.createElement('h5')
+        const p = document.createElement('p')
+        // const deleteBtn = document.createElement('button')
+    
+        containerB.className = 'block-container'
+        containerB.id = block.channel_id
+        // containerB.dataset.id = block.id
+        cardB.className = 'card-body-block'
+        h5.className = 'card-title'
+        p.className = 'card-text'
+        h5.textContent = block.title
+        p.innerText = block.description
+        // deleteBtn.innerText = 'x'
 
-    containerB.className = 'block-container'
-    containerB.id = block.channel_id
-    cardB.className = 'card-body-block'
-    h5.className = 'card-title'
-    p.className = 'card-text'
-    h5.textContent = block.title
-    p.innerText = block.description
+        // blockDiv.hidden = false
+        cardB.append(h5, p)
+        containerB.appendChild(cardB)
+        blockDiv.append(containerB)
+        blockContainer.append(blockDiv)
 
-    cardB.append(h5, p)
-    containerB.appendChild(cardB)
-    // div.appendChild(containerB)
+        // deleteBtn.addEventListener('click', deleteBlock(block, containerB.dataset.id))
+        // console.log(containerB.dataset.id)
+        // console.log(blockDiv)
 
+    }
 }
 
-function showHideDiv(e, channel_id) {
-    console.dir("e target", e.target)
-    console.log("channel id", channel_id)
-    //if div exists with blockId
-    // show div 
-// if p is clicked or body  
-//  let blockId = document.getElementById(.channel_id)
-//  if (e.style.display == null || e.style.display == "none"){
-//     e.style.display = "block";
-//  } else {
-//     e.style.display = "none"
+// function showHideDiv(e) {
+//     const div = document.getElementById(e.target.dataset.id)
+//     console.log(e.target)
+//     if (div.hidden) {
+//         div.hidden = !div.hidden
+//     } else {
+//         div.hidden = !div.hidden
+//     }
 // }
-// if (document.getElementById(counter.innerText) === null) {
-//     // create li for counter inner text
-//     let li = document.createElement("li");
-//     li.id = counter.innerText;
-//     li.data = 1;
-//     li.innerHTML =
-//       counter.innerText + " has been liked " + li.data + " times";
-//     likesUl.appendChild(li);
-//   } else {
-//     let li = document.getElementById(counter.innerText);
-//     li.data++;
-//     li.innerHTML =
-//       counter.innerText + " has been liked " + li.data + " times";
-//   }
-}
 
 //if ++channel clicked - for will populate 
-let form = document.querySelector('form')
+let channelForm = document.querySelector('.channel-form')
    const createChannel = document.querySelector('.create-channel')
    createChannel.addEventListener('click', () => {
-        if(form.hidden) {
-            form.hidden = !form.hidden
+        if(channelForm.hidden) {
+            channelForm.hidden = !channelForm.hidden
         } else {
-            form.hidden = !form.hidden
+            channelForm.hidden = !channelForm.hidden
         }
    })
 
-form.addEventListener('submit', newChannel)
-
+channelForm.addEventListener('submit', newChannel)
 function newChannel(e){
     e.preventDefault()
         let channel = {
             title: e.target.title.value
     }
+    channelForm.reset()
         postChannel(channel)
 }
 
 
+let blockForm = document.querySelector('.block-form')
+   const createBlock = document.querySelector('.create-block')
+   createBlock.addEventListener('click', () => {
+        if(blockForm.hidden) {
+            blockForm.hidden = !blockForm.hidden
+        } else {
+            blockForm.hidden = !blockForm.hidden
+        }
+   })
 
-
+blockForm.addEventListener('submit', (e) => newBlock(e))
 function newBlock(e){
+    e.preventDefault()
+    let block = {
+        title: e.target.title.value,
+        description: e.target.description.value,
+        channel_id: e.target[2].value,
+        user_id: 41
+    }
+    postBlock(block)
+    blockForm.reset()
+}
 
+function buildBlockFormDropDown(CHANNELSARR) {
+    let dropdown = document.getElementById('channelList')
+    CHANNELSARR.forEach((channel) => {
+        let options = document.createElement('option')
+        options.innerText = channel.title 
+        options.value = channel.id
+        dropdown.appendChild(options)
+    })
+    
+}
+
+function deleteBlock(block) {
+    fetch(BLOCKS_URL + `/${block.id}`, {
+        method: 'DELETE'
+    })
+        .then(res => res.json())
+        .then(() => {
+            containerB.dataset.id.remove()
+        })
 }
